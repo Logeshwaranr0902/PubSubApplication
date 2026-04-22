816b79e24eb696f830ed256e7990e39e1408ebc6
logeshwaran <logeshwaranr0902@gmail.com>
2026-04-23 01:13:33 +0530

    Added PubSub Sender

--- service1/src/main/java/com/example/service1/PubSubController.java
+++ service1/src/main/java/com/example/service1/PubSubController.java
@@ -1,14 +1,23 @@
 package com.example.service1;
 
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PubSubController {
 
-    @GetMapping("/hello")
-    public String getString() {
+    private final PubSubTemplate pubSubTemplate;
 
-        return "Hello World!";
+    public PubSubController(PubSubTemplate pubSubTemplate) {
+        this.pubSubTemplate = pubSubTemplate;
+    }
+    @GetMapping("/publish")
+    public String send(@RequestParam("msg") String message) {
+
+        this.pubSubTemplate.publish("my-first-topic", message);
+
+        return "Successfully sent: " + message;
     }
 }
