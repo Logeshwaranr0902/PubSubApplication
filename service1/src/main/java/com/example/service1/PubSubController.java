7dc32017b55c08161237b0503b8826a4a6cb280f
logeshwaran <logeshwaranr0902@gmail.com>
2026-04-23 02:31:18 +0530

    Added PubSub Sender

--- service1/src/main/java/com/example/service1/PubSubController.java
+++ service1/src/main/java/com/example/service1/PubSubController.java
@@ -1,11 +1,13 @@
 package com.example.service1;
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PubSubController {
 
     private final PubSubTemplate pubSubTemplate;
@@ -14,10 +16,14 @@ public class PubSubController {
         this.pubSubTemplate = pubSubTemplate;
     }
     @GetMapping("/publish")
-    public String send(@RequestParam("msg") String message) {
-
-        this.pubSubTemplate.publish("my-first-topic", message);
-
-        return "Successfully sent: " + message;
+    public String publish(@RequestParam("msg") String message) {
+        try {
+            String id = pubSubTemplate.publish("my-first-topic", message).get();
+            log.info("Successfully published message to GCP. ID: {}", id);
+            return "Success! ID: " + id;
+        } catch (Exception e) {
+            log.error("Failed to publish message to Pub/Sub: ", e);
+            return "Error occurred while publishing. Check server logs.";
+        }
     }
 }
