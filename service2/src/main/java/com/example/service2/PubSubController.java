207b14b09270803d404ac66d23e152f1e83f764c
logeshwaran <logeshwaranr0902@gmail.com>
2026-04-23 16:27:12 +0530

    Added pubsub receiver

---
+++ service2/src/main/java/com/example/service2/PubSubController.java
@@ -0,0 +1,41 @@
+package com.example.service2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Base64;
+
+@RestController
+@Slf4j
+public class PubSubController {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/receive")
+    public ResponseEntity<String> receive(@RequestBody String requestBody) {
+        try {
+
+            JsonNode node = objectMapper.readTree(requestBody);
+
+
+            String base64Data = node.path("message").path("data").asText();
+
+
+            String decodedMessage = new String(Base64.getDecoder().decode(base64Data));
+
+            log.info("MNC Service 2 received message: {}", decodedMessage);
+
+
+            return ResponseEntity.ok("Message Processed");
+
+        } catch (Exception e) {
+            log.error("Error decoding Pub/Sub message: ", e);
+
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+}
